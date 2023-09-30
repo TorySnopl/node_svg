6de542dfb25f6d170ad64f0caafe180b5a35ad99
TorySnopl <Paramedic064@gmail.com>
2023-09-30 11:12:48 -0700

    initial commit with prompts added

---
+++ index.js
@@ -0,0 +1,29 @@
+const inq = require('inquirer');
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt');
+const fs = require('fs');
+const shapes = require('./utilities/shapes.js');
+
+inq.registerPrompt('maxLength', MaxLengthInputPrompt);
+
+inq
+    .prompt ([
+        {
+            type: 'maxLength',
+            name: 'characters',
+            message: 'Please enter up to three characters:',
+            maxLength: 3
+        },
+        {
+            type: 'input',
+            name: 'color',
+            message: 'please enter a color keyword:'
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Please select a shape:',
+            choices: ["Circle","Triangle","Square"]
+        }
+    
+    ]);
+
