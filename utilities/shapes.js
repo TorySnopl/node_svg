2dc9694deb98e8c39f76091a5d0b424fa6992aab
TorySnopl <Paramedic064@gmail.com>
2023-09-30 14:12:41 -0700

    completed shape classes extending Shapes class constructor with the method colorChoice to set fill color

---
+++ utilities/shapes.js
@@ -0,0 +1,28 @@
+class Shapes {
+    constructor(){
+        this.color = ''
+    }
+    colorChoice(color){
+        this.color = (color);
+    }
+}
+
+class Tri extends Shapes {
+    render(){
+        return `<polygon width="300" height = "200" points ="250,60 100, 400 400, 400" fill = ${this.color}>`
+    }
+}
+
+class Sq extends Shapes {
+    render(){
+        return `<rect width="200" height="200" fill = ${this.color}>`
+    }
+}
+
+class Cir extends Shapes{
+    render(){
+        return `<circle cx="50" cy="50" r="150" fill=${this.color}>`
+    }
+}
+
+module.exports = {Tri, Sq, Cir}
